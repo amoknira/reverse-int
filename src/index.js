9dd13f98a1b82fd1b6fafab028f1a164c322cff2
amoknira <keyforgen@gmail.com>
2023-08-01 14:44:31 +0500

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
+  str = n.toString();
+  let i = 0;
+  // 
+  let result = '';
+  while (i < str.length) {
+    // link on reverse order
+    result = `${str[i]}${result}`;
+    // or concatenation:
+    // result = str[i] + result;
+    i = i + 1;
+    console.log(result);
+  }
+  // regexp for '-', g = global, search all inputs of '-'
+  return Number(result.replaceAll(/[\-]/g, ''));
 }
